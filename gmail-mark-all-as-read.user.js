0318c1b67a7f969777198d2481ae8d910ddb5173
Mark Drago <markdrago@gmail.com>
2007-11-12 18:32:56 -0500

    first shot at a user script add .gitignore to ignore emacs temp files

---
+++ gmail-mark-all-as-read.user.js
@@ -0,0 +1,15 @@
+// Gmail Mark-All-As-Read Script
+// version 0.1
+// Copyright (c) 2007 Mark Drago
+// Released under the GPL license
+// http://www.gnu.org/copyleft/gpl.html
+//
+// ==UserScript==
+// @name          Gmail Mark-All-As-Read
+// @namespace     http://www.markdrago.com/projects/gmail-mark-all-as-read/
+// @description   script to add a 'mark all as read' button to gmail
+// @include       http://mail.google.com/mail/*
+// @include       https://mail.google.com/mail/*
+// ==/UserScript==
+
+alert('Hello world!');
