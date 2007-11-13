b12bc2195370412f7653221429ba0217f0da66f0
Mark Drago <markdrago@gmail.com>
2007-11-12 20:28:19 -0500

    create gmail object correctly, write some stuff to GM_log

--- gmail-mark-all-as-read.user.js
+++ gmail-mark-all-as-read.user.js
@@ -1,15 +1,34 @@
-// Gmail Mark-All-As-Read Script
-// version 0.1
-// Copyright (c) 2007 Mark Drago
-// Released under the GPL license
+// Copyright (c) 2007, Mark Drago
+// Released under the GPL license:
 // http://www.gnu.org/copyleft/gpl.html
 //
 // ==UserScript==
-// @name          Gmail Mark-All-As-Read
-// @namespace     http://www.markdrago.com/projects/gmail-mark-all-as-read/
-// @description   script to add a 'mark all as read' button to gmail
-// @include       http://mail.google.com/mail/*
-// @include       https://mail.google.com/mail/*
+// @name          Gmail Mark All As Read
+// @namespace     http://www.markdrago.com/
+// @description   Adds a Mark All As Read button to the Gmail Threadlist
+// @include       http://mail.google.com/*
+// @include       https://mail.google.com/*
 // ==/UserScript==
 
-alert('Hello world!');
+var gmail;
+
+function view_changed () {
+  GM_log("consider the button created");
+}
+
+function main_func(gmail_obj) {
+  GM_log("got in to main_func");
+  gmail = gmail_obj;
+  gmail.registerViewChangeCallback(view_changed);
+  view_changed();
+}
+
+window.addEventListener('load', function() {
+  if (unsafeWindow.gmonkey) {
+    GM_log("in event listener");
+    unsafeWindow.gmonkey.load('1.0', function(gmail) {
+      GM_log("in gmonkey loader");
+      main_func(gmail);
+    });
+  }
+}, true);
